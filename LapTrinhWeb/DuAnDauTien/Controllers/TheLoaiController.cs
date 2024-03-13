94bd1dbfa1dd5dc047ca0ebb604ac27c2fcd11a5
KelvinNguyen2h3 <kelvin04092003@gmail.com>
2024-03-13 21:43:11 +0700

    first commit

---
+++ LapTrinhWeb/DuAnDauTien/Controllers/TheLoaiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DuAnDauTien.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "Category List: ";
+            
+            foreach (var category in categories) {
+                content += category + "; ";
+            }
+
+            return Content(content);
+        }
+    }
+}
---
+++ LapTrinhWeb/DuAnDauTien/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DuAnDauTien.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
